c0aef4cdee5c9a91f19e78cb1b4f5c8edbf1c1d6
lawebdelcolchon <105754172+lawebdelcolchon@users.noreply.github.com>
2025-02-26 09:09:34 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
+  ],
+  darkMode: ["class", '[data-theme="dark"]'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#0275bc",
+        },
+      }, "dark"
+    ],
+  },
+}
